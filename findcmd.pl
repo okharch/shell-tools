121146da000fd2e753c1e1cc3266edb0f8842f08
Oleksandr Kharchenko <okharch@gmail.com>
2014-10-14 16:50:39 +0300

    documented and -soundex search added

--- findcmd.pl
+++ findcmd.pl
@@ -4,19 +4,37 @@ use strict;
 use warnings;
 use List::MoreUtils qw(uniq);
 use Storable qw(nstore retrieve);
+use Getopt::Long;
+use Pod::Usage;
+use Text::Soundex;
 
-my $cmd_info_fn = "$ENV{HOME}/etc/cmd_info";
+my $man = 0;    
+my $help = 0;
+my $cache;
+my $soundex;
 
-my $force_rebuild = 1; # rebuild each time and don't store cache
-my ($re) = grep m{^[^-]},@ARGV;
+## Parse options and print usage if there is a syntax error,
+## or if usage was explicitly requested.
+GetOptions('help|?' => \$help, man => \$man, 'cache=s' => \$cache, soundex => \$soundex);
 
-my ($rebuild) = $force_rebuild || grep(m{^-r}, @ARGV) || !(-e $cmd_info_fn);
+my ($re) = @ARGV;
 
-my $cmd_info = $rebuild? rebuild_cmd_info() : retrieve $cmd_info_fn; # if $rebuild || !-e $cmd_info_fn;
+my $cmd_info;
+$cmd_info =  $cache && -e $cache? retrieve($cache) : rebuild_cmd_info();
+nstore $cmd_info, $cache if $cache && !-e $cache;
 
-die "please specify any sample to find" unless defined($re);
+pod2usage(1) if $help;
+pod2usage(-verbose => 2) if $man;
+pod2usage("$0: please specify any sample to find") unless defined($re);
 
 $\ = $, = "\n";
+if ($soundex) {
+    my $arg_soundex = soundex($re);
+    print $_ for @{$cmd_info->{soundex}{$arg_soundex}||[]};
+    exit 0;
+}
+
+$cmd_info = $cmd_info->{plain};
 
 for my $cmd_class (qw(alias function bin)) {
     my @cmd_name = sort keys(%{$cmd_info->{$cmd_class}});
@@ -32,13 +50,16 @@ sub fq {
 }
 
 sub rebuild_cmd_info {
-    my %cmd_info = (
+    my %plain = (
         'function' => build_function(),
         'alias' => build_alias(),
         'bin' => build_bin(),
     );
-    nstore \%cmd_info, $cmd_info_fn unless $force_rebuild;
-    return \%cmd_info;
+    my @cmd = grep defined($_->[1]), 
+    map [$_,soundex($_)], map keys(%{$plain{$_}}),qw(function alias bin);
+    my %soundex;
+    push @{$soundex{$_->[1]}},$_->[0] for @cmd;
+    return {soundex => \%soundex, plain => \%plain};
 }
 
 sub build_function {
@@ -71,3 +92,54 @@ sub build_bin {
     # we could read script content here as a lines, but don't do it to reduce search hits
     return {map {$_ => []} @bin};
 }
+
+__END__
+
+=head1 NAME
+
+findcmd.pl - build list of all bash functions, aliases, bin files from PATH and search over this names with specified regexp
+
+=head1 SYNOPSIS
+
+findcmd.pl [options] PATTERN
+
+finds regexp PATTERN over list of bash commands and their definitions (for aliases and functions)
+
+ Options:
+   -help            brief help message
+   -man             full documentation
+   -soundex         use soundex algorithm to match the pattern over command names
+   -cache=file      force to use cache in a case you have huge collection of commands and it's building on the fly takes long time.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=item B<-cache=file>
+
+It will build the cached list of bash commands and their definition.
+When cache is available it is loaded instantly and search over it.
+Use this options if builing of the list for search takes a long time.
+Usually it does not.
+
+=item B<-soundex>
+
+Use soundex algorithm to match the pattern over command names
+
+=back
+
+=head1 DESCRIPTION
+
+B<findcmd.pl> builds the list of all commands (aliases, functions, binary over PATH) that is defined in ~./profile (see bash login invocation).
+Take into account that commands defined at .bashrc (interactive invokation) are not available to search over.
+It is good if you remember there was some command that sounds like this, but do not remember it's exact spelling
+
+=cut
